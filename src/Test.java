a1de1eb6c8959abc2b68c29521824bc5ae6a2f02
GoodGithubAccount <22419372+GoodGithubAccount@users.noreply.github.com>
2023-01-28 19:35:38 +0100

    Absolutely horrible repetitive code thats not even working that great

--- src/Test.java
+++ src/Test.java
@@ -10,17 +10,6 @@ public class Test {
         int startSize = 10;
 
         crossBuilder(startSize, textArray);
-
-        for (int i = 0; i < crossArray.length; i++) {
-            for (int j = 0; j < crossArray.length; j++) {
-                if (crossArray[i][j] != '#') {
-                    System.out.print(" " + ConsoleColors.BLUE_BOLD + crossArray[i][j] + ConsoleColors.RESET + " ");
-                } else {
-                    System.out.print(" " + crossArray[i][j] + " ");
-                }
-            }
-            System.out.println();
-        }
     }
 
     static String[] crossBuilder(int size, String[] textArray) {
@@ -44,8 +33,18 @@ public class Test {
         List<String> wordList = new ArrayList<>(Arrays.asList(textArray));
         int placedWords = 0;
 
-        while (placedWords < 5) {
+        while (placedWords < 6) {
             if (tryPlace(wordList.get(0), placedWords)) {
+                for (int i = 0; i < crossArray.length; i++) {
+                    for (int j = 0; j < crossArray.length; j++) {
+                        if (crossArray[i][j] != '#') {
+                            System.out.print(" " + ConsoleColors.BLUE_BOLD + crossArray[i][j] + ConsoleColors.RESET + " ");
+                        } else {
+                            System.out.print(" " + crossArray[i][j] + " ");
+                        }
+                    }
+                    System.out.println();
+                }
                 wordList.remove(0);
                 placedWords++;
             } else {
@@ -77,10 +76,9 @@ public class Test {
                 return false; // error too maybe
             }
         } else {
-            if(tryHorizontal(word)){
+            if (tryHorizontal(word)) {
                 return true;
-            }
-            else{
+            } else {
                 return tryVertical(word);
             }
         }
@@ -92,12 +90,45 @@ public class Test {
             for (int j = 0; j < crossArray.length; j++) {
                 for (int c = 0; c < word.length(); c++) {
                     if (crossArray[i][j] == word.charAt(c)) {
+                        int countPlus = 0;
+                        int countMinus = 0;
                         for (int k = 0; k < word.length(); k++) {
                             try {
                                 if (crossArray[i + k - c][j] != '#' && crossArray[i + k - c][j] != word.charAt(k)) {
                                     return false;
                                 }
                             } catch (ArrayIndexOutOfBoundsException e) {
+                                return false;
+                            }
+
+
+                            Boolean[] result = verifyPosition((i + k - c), j, word.charAt(k), false);
+
+                            if(crossArray[i + k - c][j] == word.charAt(k)){
+                                if(!result[0]){
+                                    countPlus++;
+                                }
+                                else{
+                                    countPlus = 0;
+                                }
+
+                                if(!result[1]){
+                                    countMinus++;
+                                }
+                                else{
+                                    countMinus = 0;
+                                }
+                            }
+                            else if(crossArray[i + k - c][j] == '#' && result[0] && result[1]){
+                                countPlus = 0;
+                                countMinus = 0;
+                            }
+                            else{
+                                return false;
+                            }
+
+                            if(countPlus >= 2 || countMinus >= 2){
+                                System.out.println("HOW???");
                                 return false;
                             }
                         }
@@ -112,17 +143,83 @@ public class Test {
         }
         return false;
     }
+
+    static Boolean[] verifyPosition(int x, int y, char letter, boolean isHorizontal) {
+        int xPlus = 0;
+        int yPlus = 0;
+        if (isHorizontal) {
+            yPlus = 1;
+        } else {
+            xPlus = 1;
+        }
+
+        boolean flagPlus = false;
+        boolean flagMinus = false;
+
+
+        if (y + yPlus > crossArray.length || x + xPlus > crossArray.length ||
+                y + yPlus < crossArray.length || x + xPlus < crossArray.length) {
+            flagPlus = true;
+        } else if (crossArray[x + xPlus][y + yPlus] == '#') {
+            flagPlus = true;
+        }
+
+        if (y - yPlus > crossArray.length || x - xPlus > crossArray.length ||
+                y - yPlus < crossArray.length || x - xPlus < crossArray.length) {
+            flagMinus = true;
+        } else if (crossArray[x - xPlus][y - yPlus] == '#') {
+            flagMinus = true;
+        }
+
+        return new Boolean[]{flagPlus, flagMinus};
+    }
+
+
     static Boolean tryHorizontal(String word) {
         for (int i = 0; i < crossArray.length; i++) {
             for (int j = 0; j < crossArray.length; j++) {
                 for (int c = 0; c < word.length(); c++) {
                     if (crossArray[i][j] == word.charAt(c)) {
+                        int countPlus = 0;
+                        int countMinus = 0;
+
                         for (int k = 0; k < word.length(); k++) {
                             try {
                                 if (crossArray[i][j + k - c] != '#' && crossArray[i][j + k - c] != word.charAt(k)) {
                                     return false;
                                 }
                             } catch (ArrayIndexOutOfBoundsException e) {
+                                return false;
+                            }
+
+                            Boolean[] result = verifyPosition(i, (j + k - c), word.charAt(k), true);
+
+                            if(crossArray[i][j + k - c] == word.charAt(k)){
+                                if(!result[0]){
+                                    countPlus++;
+                                }
+                                else{
+                                    countPlus = 0;
+                                }
+
+                                if(!result[1]){
+                                    countMinus++;
+                                }
+                                else{
+                                    countMinus = 0;
+                                }
+                            }
+                            else if(crossArray[i][j + k - c] == '#' && result[0] && result[1]){
+                                countPlus = 0;
+                                countMinus = 0;
+                            }
+                            else{
+                                return false;
+                            }
+
+
+                            if(countPlus >= 2 || countMinus >= 2){
+                                System.out.println("HOW???");
                                 return false;
                             }
                         }
