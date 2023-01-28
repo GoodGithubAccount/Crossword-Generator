605bf2c4a223894ae60f33be01824e7e7061659d
GoodGithubAccount <22419372+GoodGithubAccount@users.noreply.github.com>
2023-01-28 14:37:52 +0100

    First word placement

--- src/Test.java
+++ src/Test.java
@@ -1,21 +1,27 @@
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 public class Test {
-
+    static char crossArray[][] = {};
     public static void main(String[] args) {
-        String textData = "one-two-three-four-five-six-seven-eight-nine-ten".toLowerCase();
+        String textData = "one-two-three-four-five-six-seven-eight-nine-ten".toUpperCase();
         String[] textArray = textData.split("-");
 
         int startSize = 10;
 
         crossBuilder(startSize, textArray);
-
     }
 
     static String[] crossBuilder(int size, String[] textArray){
-        String[][] crossArray = new String[size][size];
+        crossArray = new char[size][size];
+
+        for (int i = 0; i < crossArray.length; i++)   {
+            for (int j = 0; j < crossArray.length; j++)   {
+                crossArray[i][j] = '#';
+            }
+        }
 
         Random random = new Random(System.currentTimeMillis());
         for(int i = 0; i < textArray.length; i++){
@@ -25,18 +31,50 @@ public class Test {
             textArray[i] = textArray[indexToSwap];
             textArray[indexToSwap] = tempString;
         }
-
         List<String> wordList = Arrays.stream(textArray).toList();
-
-        //ss
+        int placedWords = 0;
+        tryPlace(wordList.get(0), placedWords);
 
         for(int i = 0; i < textArray.length; i++){
             System.out.println(textArray[i]);
         }
 
+        for (int i = 0; i < crossArray.length; i++)   {
+            for (int j = 0; j < crossArray.length; j++)   {
+                if(crossArray[i][j] != '#'){
+                    System.out.print(" " + ConsoleColors.BLUE_BOLD + crossArray[i][j] + ConsoleColors.RESET + " ");
+                }
+                else{
+                    System.out.print(" " + crossArray[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+
         return null;
     }
 
+    static Boolean tryPlace(String word, int placedWords){
+        if (placedWords == 0){
+            if(word.length() <= crossArray.length){
+                int position = (crossArray.length - word.length()) / 2;
+                for (int i = 0; i < word.length(); i++){
+                    // Change it to be random direction later
+                    crossArray[i+position][position] = word.charAt(i);
+                }
+            }
+            else{
+                return false; // error too maybe
+            }
+        }
+        else{
+            for (int i = 0; i < crossArray.length; i++)   {
+                for (int j = 0; j < crossArray.length; j++)   {
 
+                }
+            }
+        }
 
+        return false;
+    }
 }
